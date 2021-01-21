5dcb3053cf86690aa4fa53261dfb61c2111e9ffa
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2021-01-20 17:45:53 -0700

    Working on MVP

---
+++ redux-practice-app/src/reducers/index.js
@@ -0,0 +1,14 @@
+
+const initialState = {
+  coords: '',
+  error: ''
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default reducer;
